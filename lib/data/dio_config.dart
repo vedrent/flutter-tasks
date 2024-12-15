8a27a8cb6066bbf38e76e637f33c992420c70f76
vedro <vedrentoff@gmail.com>
2024-12-15 14:11:38 +0300

    branch init

--- lib/data/dio_config.dart
+++ lib/data/dio_config.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 Dio getHttpClient() {
   BaseOptions options = BaseOptions(
-      baseUrl: "http://192.168.100.114:8080",
+      baseUrl: "http://192.168.1.37:8080",
       responseType: ResponseType.plain,
       validateStatus: (code) {
         if (code == null) {
